43ef2f7bfc0d6af0e8ec3e7e656b9ba1b4c5913d
Rob Tweed <rtweed@mgateway.com>
2014-02-27 18:12:56 +0000

    Build 1.1: Ready for use

--- example/VistaTerm.js
+++ example/VistaTerm.js
@@ -2,20 +2,28 @@ var term = require('ewdvistaterm');
 
 term.start({
   log: true,
-  spawnVistA: {
-    command: 'ssh',
+  spawnVistA: { // how to start a back-end terminal connection/process
+    command: 'ssh', 
     arguments: ['-q', 'steve@127.0.0.1', '-tt', '-i', '/home/ubuntu/steve']
+
+    // this example is the equivalent of ssh -q 'steve@127.0.0.1 -tt -i '/home/ubuntu/steve'
+    //  note: -tt is needed to force tty allocation
+    //  in the example above I'm using an authentication key
+    //  if you need to use a password, install sshpass instead and this as an example:
+    //     command: 'sshpass',
+    //     arguments: ['-p', 'vistaewd', 'ssh', '-o', 'StrictHostKeyChecking=no', 'vista@127.0.0.1', '-tt']
+
   },
   webServer: {
-    port: 8081,
-    rootPath: '/home/ubuntu/www',
-    ssl: true,
+    port: 8081,  // port on which web server will listen
+    rootPath: '/home/ubuntu/www',   // physical path that represents the web server's root path
+    ssl: true,  
     options: {
       key: '/home/ubuntu/ssl/ssl.key',
       cert: '/home/ubuntu/ssl/ssl.crt'
     }
   },
-  addLF: false,
-  echo: true,
-  exitString: 'Logged out at '
+  addLF: false,  // for Linux systems, this should be false
+  echo: true,    // for Linux systems, this should be true
+  exitString: 'Logged out at '  // string that tells the terminal to no longer communicate with the back end
 });
